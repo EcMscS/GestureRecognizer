1d19714f38c5af1f0d59b177eecadd5a5051c078
Jeff Lai <jefflai@gmail.com>
2019-11-12 15:18:26 -0800

    Completed PanGesture
    Pan gesture using Storyboard

--- GestureRecognizer/Controllers/ViewController.swift
+++ GestureRecognizer/Controllers/ViewController.swift
@@ -10,11 +10,76 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fileImageView: UIImageView!
+    @IBOutlet weak var trashImageView: UIImageView!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    var fileViewOrigin:CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initialSetup()
+        addPanGesture(view: fileImageView)
     }
 
+    func initialSetup() {
+        refreshButton.isHidden = true
+        fileViewOrigin = fileImageView.frame.origin
+        view.bringSubviewToFront(fileImageView)
+    }
+    
+    func addPanGesture(view: UIView) {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePan(sender:)))
+        view.addGestureRecognizer(pan)
+    }
 
+    @objc func handlePan(sender: UIPanGestureRecognizer) {
+        let fileView = sender.view!
+        
+        switch sender.state {
+        case .began, .changed:
+            moveViewWithPan(view: fileView, sender: sender)
+        case .ended:
+            if fileView.frame.intersects(trashImageView.frame) {
+                deleteView(view: fileView)
+            } else {
+                returnViewToOrigin(view: fileView)
+            }
+        default:
+            break
+        }
+    }
+    
+    func moveViewWithPan(view: UIView, sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: view)
+    }
+    
+    func returnViewToOrigin(view: UIView) {
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseIn, animations: {
+            self.fileImageView.frame.origin = self.fileViewOrigin
+        }, completion: nil)
+    }
+    
+    func deleteView(view: UIView) {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.fileImageView.alpha = 0.0
+        }) { _ in
+            self.refreshButton.isHidden = false
+        }
+    }
+    
+    func resetFileImage() {
+        fileImageView.frame.origin = fileViewOrigin
+        fileImageView.alpha = 1.0
+    }
+    
+    @IBAction func refreshButtonPressed(_ sender: UIButton) {
+        refreshButton.isHidden = true
+        resetFileImage()
+    }
+    
 }
 
